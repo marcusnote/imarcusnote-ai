c5e2a9e79c79d555fcb70e2c43f91056bceb9a27
marcusnote <marcusnote.official@gmail.com>
2026-03-29 02:26:45 +0900

    Update generate-pdf.js

--- api/generate-pdf.js
+++ api/generate-pdf.js
@@ -1,3 +1,6 @@
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
 
